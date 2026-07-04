7bc1de654c453594f35db9090e5c0f0037581b25
roroanro459-lab <roroanro459@gmail.com>
2026-07-04 12:59:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Display = document.getElementById("Display");
+
+function appendToDisplay(input){
+   Display.value += input;
+}
+
+function clearDisplay(){
+   Display.value = "";
+}
+
+function calculate(){
+   try{
+    Display.value = eval(Display.value);
+   }
+   catch(error){
+    Display.value = "Syntax Error"
+   }
+}
